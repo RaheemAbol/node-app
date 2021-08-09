a96be365b64a9cc559263e199c70548896b058c3
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2021-08-09 14:02:15 -0400

    v.1 basic online store

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type : Sequelize.INTEGER,
+        autoIncrement : true,
+        allowNull : true,
+        primaryKey : true
+    },
+    name: Sequelize.STRING,
+    email: Sequelize.STRING
+
+});
+
+
+module.exports = User;
